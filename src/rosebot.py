ca7c0a36e3fa2bcb04c0c8573ed11784d922d6b1
JingyiJia <jiaj@rose-hulman.edu>
2019-02-06 16:00:11 -0500

    Added names.

--- src/rosebot.py
+++ src/rosebot.py
@@ -6,7 +6,7 @@
     -- Lower-level code to interact with the EV3 robot library.
 
   Author:  Your professors (for the framework and lower-level code)
-    and Jiadi.Wang, Chen,Li, Jingyi,Jia.
+    and Jiadi.Wang, Jingyi,Jia, Li.Chen.
   Winter term, 2018-2019.
 """
 
--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -3,7 +3,7 @@
   This code is the delegate for handling messages from the shared GUI.
 
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAMES_HERE.
+    and Jiadi. Wang, Jingyi.Jia, Chen.Li.
   Winter term, 2018-2019.
 """
 
