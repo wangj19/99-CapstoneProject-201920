bd0f1c32183631cf1e7083fef44a94e02a8be38f
wangj19 <wangj19@rose-hulman.edu>
2019-02-07 16:31:49 -0500

    Hugo

--- src/rosebot.py
+++ src/rosebot.py
@@ -79,6 +79,7 @@ class DriveSystem(object):
         self.left_motor.turn_on(left_wheel_speed)
         self.right_motor.turn_on(right_wheel_speed)
     def stop(self):
+
         """ Stops the left and right wheel motors. """
 
     def go_straight_for_seconds(self, seconds, speed):
