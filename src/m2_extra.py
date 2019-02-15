37b7d906469fdd7cb67502d4880d5ce40db8d70f
wangj19 <wangj19@rose-hulman.edu>
2019-02-15 16:35:25 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/shared_gui_delegate_on_robot.py

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -4,18 +4,13 @@ import rosebot
 
 
 
-
-
-
-
-
 def get_closer_tone():
     robot = rosebot.RoseBot()
     robot.drive_system.go(50, 50)
     while True:
         distance = robot.sensor_system.ir_proximity_sensor.get_distance()
         if distance > 40:
-            robot.sound_system.tone_maker.play_tone(distance*3, 200)
+            robot.sound_system.tone_maker.play_tone(distance*3, 100)
         else:
             robot.drive_system.stop()
             robot.sound_system.tone_maker.play_tone(0, 0).wait()
