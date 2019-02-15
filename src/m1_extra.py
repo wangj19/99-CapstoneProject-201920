38af0761ea4d97146e82f961acc146a490bc3e2f
wangj19 <wangj19@rose-hulman.edu>
2019-02-15 11:53:49 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/shared_gui_delegate_on_robot.py

--- src/m1_extra.py
+++ src/m1_extra.py
@@ -3,10 +3,10 @@ import time
 
 def fasterbeep():
     robot = rosebot.RoseBot()
-    robot.drive_system.go(50,50)
+    robot.drive_system.go(30,30)
     while True:
         distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-        delay =distance/100
+        delay =distance/200
         robot.sound_system.tone_maker.play_tone(400,300)
         time.sleep(delay)
         if distance < 50:
