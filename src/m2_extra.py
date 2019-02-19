4eee49d7b653e67dc78de62a003c36d04acfd9db
JingyiJia <jiaj@rose-hulman.edu>
2019-02-18 21:48:22 -0500

    Sprint 3 Individual Part

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -1,4 +1,4 @@
-import shared_gui_delegate_on_robot as rec
+
 import rosebot
 import time
 
@@ -18,74 +18,83 @@ def get_closer_tone():
             break
 
 delay = 400
-robot = rosebot.RoseBot
-receiver = rec.Receiver(robot)
-distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-task_complete = 0
 
 def final_week():
-    robot.drive_system.go(30,30)
-    if task_complete == 5:
-        robot.drive_system.go(30,30)
+
+    task_complete = 0
+    robot = rosebot.RoseBot()
+    distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+    robot.sound_system.speech_maker.speak('Final week starts')
+
+    if robot.sensor_system.color_sensor.get_reflected_light_intensity == 1 & abs(task_complete) == 5:
+        robot.drive_system.go(30, 30)
         if distance < 30:
-            robot.sound_system.speech_maker.speak('End of Final Week')
+            robot.drive_system.stop()
             robot.arm_and_claw.raise_arm()
+            robot.sound_system.speech_maker.speak('End of Final Week')
+            robot.arm_and_claw.lower_arm()
 
-
-
-
-
-def math_final_exam(task_complete):
     if robot.sensor_system.color_sensor.get_reflected_light_intensity == 1:
-        robot.sound_system.speech_maker.speak('Math Final Exam')
-
-        while True:
-            robot.drive_system.stop()
-            time.sleep(delay)
-            robot.sound_system.speech_maker.speak('Finish')
-            task_complete = task_complete + 1
-            break
-    return task_complete
-
-
-def physics_final_exam(task_complete):
-    if robot.sensor_system.color_sensor.get_reflected_light_intensity == 2:
-        robot.sound_system.speech_maker.speak('Physics Final Exam')
-
-        while True:
-            robot.drive_system.stop()
-            time.sleep(delay)
-            robot.sound_system.speech_maker.speak('Finish')
-            task_complete = task_complete + 1
-            break
-    return task_complete
-
-
-def ece_final_exam(task_complete):
-    if robot.sensor_system.color_sensor.get_reflected_light_intensity == 3:
-        robot.sound_system.speech_maker.speak('ECE Final Exam')
-
-        while True:
-            robot.drive_system.stop()
-            time.sleep(delay)
-            robot.sound_system.speech_maker.speak('Finish')
-            task_complete = task_complete + 1
-            break
-    return task_complete
+        robot.sound_system.speech_maker.speak('Did not complete all tasks')
 
 
 
+    def math_final_exam(task_complete):
+        if robot.sensor_system.color_sensor.get_reflected_light_intensity == 3:
+            robot.sound_system.speech_maker.speak('Math Final')
 
+            while True:
+                robot.drive_system.stop()
+                time.sleep(delay)
+                robot.sound_system.tone_maker(500, 500)
+                task_complete = task_complete + 1
+                break
+        return math_final_exam(task_complete)
 
+    def physics_final_exam(task_complete):
+        if robot.sensor_system.color_sensor.get_reflected_light_intensity == 5:
+            robot.sound_system.speech_maker.speak('Physics Final')
 
+            while True:
+                robot.drive_system.stop()
+                time.sleep(delay)
+                robot.sound_system.tone_maker(500, 500)
+                task_complete = task_complete + 1
+                break
+        return physics_final_exam(task_complete)
 
+    def ece_final_exam(task_complete):
+        if robot.sensor_system.color_sensor.get_reflected_light_intensity == 4:
+            robot.sound_system.speech_maker.speak('Electric Engineering Final')
 
+            while True:
+                robot.drive_system.stop()
+                time.sleep(delay)
+                robot.sound_system.tone_maker(500, 500)
+                task_complete = task_complete + 1
+                break
+        return ece_final_exam(task_complete)
 
+    def chemistry_final_exam(task_complete):
+        if robot.sensor_system.color_sensor.get_reflected_light_intensity == 2:
+            robot.sound_system.speech_maker.speak('Chemistry')
 
+            while True:
+                robot.drive_system.stop()
+                time.sleep(delay)
+                robot.sound_system.tone_maker(500, 500)
+                task_complete = task_complete + 1
+                break
+        return chemistry_final_exam(task_complete)
 
+    def csse_project(task_complete):
+        if robot.sensor_system.color_sensor.get_reflected_light_intensity == 1:
+            robot.sound_system.speech_maker.speak('Computer Science')
 
-
-
-
-
-
+            while True:
+                robot.drive_system.stop()
+                time.sleep(delay)
+                robot.sound_system.tone_maker(500, 500)
+                task_complete = task_complete + 1
+                break
+        return csse_project(task_complete)
