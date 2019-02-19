f2c2cd524e3380837a0263851d88c985feb6e4f6
JingyiJia <jiaj@rose-hulman.edu>
2019-02-18 23:02:09 -0500

    Sprint 3 Individual Part

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -5,17 +5,17 @@ import time
 
 
 
-def get_closer_tone():
-    robot = rosebot.RoseBot()
-    robot.drive_system.go(50, 50)
-    while True:
-        distance = robot.sensor_system.ir_proximity_sensor.get_distance()
-        if distance > 20:
-            robot.sound_system.tone_maker.play_tone(distance*3, 100)
-        else:
-            robot.drive_system.stop()
-            robot.sound_system.tone_maker.play_tone(0, 0).wait()
-            break
+#def get_closer_tone():
+    #robot = rosebot.RoseBot()
+   # robot.drive_system.go(50, 50)
+   # while True:
+       # distance = robot.sensor_system.ir_proximity_sensor.get_distance()
+       # if distance > 20:
+       #     robot.sound_system.tone_maker.play_tone(distance*3, 100)
+       # else:
+         #   robot.drive_system.stop()
+         #   robot.sound_system.tone_maker.play_tone(0, 0).wait()
+         #   break
 
 delay = 400
 
