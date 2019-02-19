b74ad461b31be93b7c01a277fd1528b02668e97e
JingyiJia <jiaj@rose-hulman.edu>
2019-02-18 21:11:33 -0500

    Sprint 3 Individual Part

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -1,5 +1,6 @@
-
+import shared_gui_delegate_on_robot as rec
 import rosebot
+import time
 
 
 
@@ -15,5 +16,76 @@ def get_closer_tone():
             robot.drive_system.stop()
             robot.sound_system.tone_maker.play_tone(0, 0).wait()
             break
+
+delay = 400
+robot = rosebot.RoseBot
+receiver = rec.Receiver(robot)
+distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+task_complete = 0
+
+def final_week():
+    robot.drive_system.go(30,30)
+    if task_complete == 5:
+        robot.drive_system.go(30,30)
+        if distance < 30:
+            robot.sound_system.speech_maker.speak('End of Final Week')
+            robot.arm_and_claw.raise_arm()
+
+
+
+
+
+def math_final_exam(task_complete):
+    if robot.sensor_system.color_sensor.get_reflected_light_intensity == 1:
+        robot.sound_system.speech_maker.speak('Math Final Exam')
+
+        while True:
+            robot.drive_system.stop()
+            time.sleep(delay)
+            robot.sound_system.speech_maker.speak('Finish')
+            task_complete = task_complete + 1
+            break
+    return task_complete
+
+
+def physics_final_exam(task_complete):
+    if robot.sensor_system.color_sensor.get_reflected_light_intensity == 2:
+        robot.sound_system.speech_maker.speak('Physics Final Exam')
+
+        while True:
+            robot.drive_system.stop()
+            time.sleep(delay)
+            robot.sound_system.speech_maker.speak('Finish')
+            task_complete = task_complete + 1
+            break
+    return task_complete
+
+
+def ece_final_exam(task_complete):
+    if robot.sensor_system.color_sensor.get_reflected_light_intensity == 3:
+        robot.sound_system.speech_maker.speak('ECE Final Exam')
+
+        while True:
+            robot.drive_system.stop()
+            time.sleep(delay)
+            robot.sound_system.speech_maker.speak('Finish')
+            task_complete = task_complete + 1
+            break
+    return task_complete
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
